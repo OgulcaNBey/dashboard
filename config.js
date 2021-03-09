1850c81752c772d5ae4449e25fa7de89f065b40c
OgulcaNBey <62170184+OgulcaNBey@users.noreply.github.com>
2021-03-09 17:58:53 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  "token": "NzExNjkzNjI1MjMwMzYwNTc2.XxWdog.5bekVcAUItvwqCnuPKrlas3DxAY", //botunuzun tokenini yazınız
+  "dashboard" : {
+    "oauthSecret": "h_wcbCZJ_DN3M6gRtLsQA1_-tEgvPWcX", //botunuzun secretini yazınız 
+    "callbackURL": `https://dashboard-s.glitch.me/callback`,//site URLnizi yazınız /callback kısmını silmeyiniz!
+    "sessionSecret": "super-secret-session-thing", //kalsın
+    "domain": "https://dashboard-s.glitch.me/",
+     "port": 8000
+  }
+};
